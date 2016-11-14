d18872770cffbf9613b93f02f4e1b06c3665e617
unknown <felipe.soares@PC040.opencorporate.com.br>
2016-11-14 17:11:33 -0200

    Project start

---
+++ config.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "adodb5/adodb.inc.php";
+
+$CFG = array(
+    "db_host" => "localhost",
+    "db_user" => "root",
+    "db_password" => "admin",
+    "db_name" => "gdsp"
+);
+
+$CFG = (object) $CFG;
+
+$DB = NewADOConnection("mysqli");
+$DB->Connect($CFG->db_host, $CFG->db_user, $CFG->db_password, $CFG->db_name) or die("<h1>Falha na conex&atilde;o!</h1>");
+$DB->EXECUTE("SET NAMES 'utf8'");
+
+#$DB->debug = true;
+
+date_default_timezone_set ( "America/Sao_Paulo" );
+
+ini_set ( "display_errors", 1 );
+ini_set ( "display_startup_erros", 1 );
+error_reporting ( E_ALL );
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+  //require 'config.inc.php';
+  require 'vendor/autoload.php';
+
+  $app = new \Slim\Slim();
+
+  $app->get('/hello/:name', function ($name) {
+      echo "Hello, $name";
+  });
+
+  $app->run();
